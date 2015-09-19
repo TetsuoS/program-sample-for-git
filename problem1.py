d34aee47f33dd1318f1cf17c8321e444b04b1492
Tetsuo Saito <administrator@nenryo10.net>
2015-09-19 17:04:23 +0900

    Euler Problem 1wq

---
+++ problem1.py
@@ -0,0 +1,21 @@
+i = 0
+j = 0
+while i < 1000:
+   j = j + 3*i
+   i = i + 1
+
+k = 0
+l = 0
+while k < 1000:
+   l = l + 5*k
+   k = k + 1
+
+m = 0
+n = 0
+while m < 1000:
+   n = n + 15*m
+   m = m + 1
+
+ans = j + l - n
+
+print(ans)
